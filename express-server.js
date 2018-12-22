999988fe84d954ddb3f56d4188612fac8497b98c
John Lennie <johnlennie@Johns-MacBook-Pro.local>
2018-12-22 00:34:36 -0500

    switched to hashed passwords

--- express-server.js
+++ express-server.js
@@ -1,37 +1,74 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let users = {
-  "7ef29t": {
-    "id": "7ef29t",
+let userDatabase = {
+  "l21nf1": {
+    "id": "l21nf1",
+    "name": "John Lennie",
     "email": "johnanthonylennie@gmail.com",
-    "password": "Applesauces1"
+    "password": "$2b$10$CjAoOMLv3gPVqH0Q.ENdQ.wTJs538NJQoDHcedxpTVyGfI7Sdcsq."
   }
 }
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "b2xVn2": {
+    "userID": "l21nf1",
+    "url": "http://www.lighthouselabs.ca"
+  },
+  "9sm5xK": {
+    "userID": "7ef29t",
+    "url": "http://www.google.com"
+  },
+  "fc8gnb": {
+    "userID": "36m44b",
+    "url": "https://expressjs.com/en/api.html#req.get"
+  }
+}
 
 let templateVars = {
+  users: userDatabase,
   urls: urlDatabase,
-  user: {}
+  currentUser: "",
+  currentUserUrls: {},
+  currentUserHasUrls: false,
+  isUserLoggedIn: false
 };
 
-console.log(`Starting up app...`);
-console.log(`users: ${JSON.stringify(users, null, 2)}`);
+console.log(`Starting up server...`);
 console.log(`templateVars: ${JSON.stringify(templateVars, null, 2)}`);
 
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
 
+function setUser(req) {
+  if (req.cookies['userId']) {
+    templateVars.isUserLoggedIn = true;
+    templateVars.currentUser = req.cookies['userId'];
+  }
+}
+
+function urlsForUser(id) {
+  console.log(id);
+  for (var key in urlDatabase) {
+    if (urlDatabase.hasOwnProperty(key)) {
+      if (id === urlDatabase[key].userID) {
+        templateVars.currentUserUrls[key] = urlDatabase[key].url;
+        templateVars.currentUserHasUrls = true;
+      }
+    }
+  }
+}
+
 app.get("/register", (req, res) => {
+  setUser(req);
   res.render("urls-register", templateVars);
 });
 
@@ -40,45 +77,38 @@ app.post("/register", (req, res) => {
     res.status(400).send("email or passowrd fields can't be blank")
     return
   }
-  for (var key in users) {
-    if (users.hasOwnProperty(key)) {
-      if (users[key].email === req.body.email) {
+  for (var key in userDatabase) {
+    if (userDatabase.hasOwnProperty(key)) {
+      if (userDatabase[key].email === req.body.email) {
         res.status(400).send("email already exists")
         return
       }
     }
   }
   var userId = generateRandomString();
-  users[userId] = {
+  userDatabase[userId] = {
     id: userId,
+    name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: bcrypt.hashSync(req.body.password, 10)
   }
-  templateVars.user = {
-    id: userId,
-    email: req.body.email,
-    password: req.body.password
-  }
-  console.log(`users: ${JSON.stringify(users, null, 2)}`);
+  templateVars.currentUser = userId;
   console.log(`templateVars: ${JSON.stringify(templateVars, null, 2)}`);
   res.cookie('userId', userId);
   res.redirect(302, `/urls`);
 });
 
 app.get("/login", (req, res) => {
+  setUser(req);
   res.render("urls-login", templateVars);
 });
 
 app.post("/login", (req, res) => {
   var match = false;
-  for (var key in users) {
-    if (users.hasOwnProperty(key)) {
-      if (users[key].email === req.body.email && users[key].password === req.body.password) {
-        templateVars.user = {
-          id: key,
-          email: users[key].email,
-          password: users[key].password
-        }
+  for (var key in userDatabase) {
+    if (userDatabase.hasOwnProperty(key)) {
+      if (userDatabase[key].email === req.body.email && bcrypt.compareSync(req.body.password, userDatabase[key].password)) {
+        templateVars.currentUser = key;
         match = true;
         res.cookie('userId', key);
       }
@@ -94,32 +124,54 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie('userId');
-  templateVars.user = {};
+  templateVars.currentUser = "";
+  templateVars.isUserLoggedIn = false;
+  templateVars.currentUserUrls = {};
   console.log(`templateVars: ${JSON.stringify(templateVars, null, 2)}`);
   res.redirect(302, `/urls`);
 });
 
 app.get("/urls", (req, res) => {
+  setUser(req);
+  urlsForUser(req.cookies['userId']);
+  console.log(`templateVars: ${JSON.stringify(templateVars, null, 2)}`);
   res.render("urls-index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   var shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(302, `/urls/${shortURL}`);
+  console.log(urlDatabase[shortURL]);
+  urlDatabase[shortURL] = {
+    userID: req.cookies['userId'],
+    url: req.body.longURL
+  }
+  res.redirect(302, `/urls`);
 });
 
 app.get("/urls/new", (req, res) => {
+  setUser(req);
+  if (templateVars.isUserLoggedIn === false) {
+    res.redirect(302, `/login`);
+  }
   res.render("urls-new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+  setUser(req);
+  urlsForUser(req.cookies['userId']);
+  if (templateVars.urls[req.params.id].userID !== templateVars.currentUser) {
+    res.status(403).send("url does not belong to you")
+  }
   templateVars.shortURL = req.params.id;
+  console.log(`templateVars: ${JSON.stringify(templateVars, null, 2)}`);
   res.render("urls-show", templateVars);
 });
 
-app.post("/urls/:id", (req, res) => {
+app.post("/urls/:id/update", (req, res) => {
+  var shortURL = req.params.id;
+  var updatedLongURL = req.body.updatedLongURL;
+  urlDatabase[shortURL] = updatedLongURL;
+  res.redirect(302, `/urls`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
